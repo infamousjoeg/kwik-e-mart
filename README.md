5713e52dcc8ec336e234d51e684415b90fe13883
Joe Garcia <joe@joe-garcia.com>
2021-11-03 19:54:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Qwik-E-Mart Demo App <!-- omit in toc -->
+
+A simple Golang-based application that queries a PostgreSQL database named `qwikemart` to read and return customer data stored in the `customers` table.
+
+- [Database Structure](#database-structure)
+- [Required Environment Variables](#required-environment-variables)
+- [Run with Summon](#run-with-summon)
+  - [Pre-Requisites](#pre-requisites)
+  - [Command](#command)
+- [Build Binary From Source](#build-binary-from-source)
+  - [Build for current OS and OS Architecture](#build-for-current-os-and-os-architecture)
+  - [Build for different OS and OS Architecture](#build-for-different-os-and-os-architecture)
+- [License](#license)
+
+## Database Structure
+
+Database Name: `qwikemart`
+Table Name: `customers`
+
+|id|first_name|last_name|pmt_type|
+|-|-|-|-|
+|1|Homer|Simpson|cash|
+|2|Montgomery|Burns|credit|
+|3|Barney|Gumble|debit|
+|4|Waylon|Smithers|cash|
+|5|Ned|Flanders|credit|
+
+## Required Environment Variables
+
+|variable name|expected value|
+|-|-|
+|DB_HOST|hostname to PostgreSQL database|
+|DB_USERNAME|username to use for authentication|
+|DB_PASSWORD|password to use for authentication|
+|DB_NAME|name of the database to connect to|
+
+## Run with Summon
+
+### Pre-Requisites
+
+* [Summon](https://cyberark.github.io/summon)
+* [Supported Summon Provider](https://cyberark.github.io/summon/#providers)
+* [secrets.yml](secrets.yml) file
+
+### Command
+
+```shell
+summon -p $provider_name go run main.go
+```
+
+## Build Binary From Source
+
+### Build for current OS and OS Architecture
+
+```shell
+go build .
+```
+
+### Build for different OS and OS Architecture
+
+```shell
+GOOS=windows GOARCH=amd64 go build .
+```
+
+* `GOOS` is the operating system name
+* `GOARCH` is the architecture to compile for
+
+## License
+
+[MIT](LICENSE)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	host     = os.Getenv("DB_HOST")
+	port     = 5432
+	user     = os.Getenv("DB_USERNAME")
+	password = os.Getenv("DB_PASSWORD")
+	dbname   = os.Getenv("DB_NAME")
+	emptyEnv []string
+)
+
+func checkEnvValues() error {
+	if host == "" {
+		emptyEnv = append(emptyEnv, "DB_HOST")
+	}
+	if user == "" {
+		emptyEnv = append(emptyEnv, "DB_USERNAME")
+	}
+	if password == "" {
+		emptyEnv = append(emptyEnv, "DB_PASSWORD")
+	}
+	if dbname == "" {
+		emptyEnv = append(emptyEnv, "DB_NAME")
+	}
+	if emptyEnv == nil {
+		return nil
+	}
+
+	return fmt.Errorf("required environment variable(s) missing: %s", strings.Join(emptyEnv, ","))
+}
+
+func main() {
+	// Check required environment variable values
+	err := checkEnvValues()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	// Database connection string
+	psqlConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+
+	// Open database
+	db, err := sql.Open("qwikemart", psqlConn)
+	if err != nil {
+		log.Fatalf("an error occured while connecting to database: %s", err)
+	}
+
+	// Close database
+	defer db.Close()
+
+	// Query database for returned rows
+	rows, err := db.Query(`SELECT "id", "first_name", "last_name", "pmt_type" FROM "customers"`)
+	if err != nil {
+		log.Fatalf("error while querying database: %s", err)
+	}
+
+	// Close query connection
+	defer rows.Close()
+
+	fmt.Println("id, first_name, last_name, pmt_type")
+	fmt.Println("-----------------------------------")
+
+	// Print returned rows
+	for rows.Next() {
+		var id int
+		var first_name string
+		var last_name string
+		var pmt_type string
+
+		err = rows.Scan(&id, &first_name, &last_name, &pmt_type)
+		if err != nil {
+			log.Fatalf("error scanning returned row data: %s", err)
+		}
+
+		fmt.Printf("%d, %s, %s, %s", id, first_name, last_name, pmt_type)
+	}
+}
